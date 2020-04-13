bedbc80077815f45cd5207ff76eed1b42df860cc
Anna <anyka69@mail.ru>
2020-04-13 13:59:13 +0300

    Test MoodModal

--- src/test/java/pages/MoodModal.java
+++ src/test/java/pages/MoodModal.java
@@ -1,8 +1,8 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.openqa.selenium.Keys;
-
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,20 +13,22 @@ public class MoodModal {
         $(UPDATE_BUTTON_CSS).should(Condition.appear);
         return this;
     }
+
+    @Step("Mood Change")
     public MoodModal updateDetails(int moodRating,String description, String date){
-            $("#TextBoxUpdateMoodTag").sendKeys(description);
-            if(moodRating>5){
-            for (int i=0; i<moodRating-5;i++) {
+        $("#TextBoxUpdateMoodTag").sendKeys(description);
+        if(moodRating>5){
+            for (int i=0; i<moodRating-5; i++) {
                 $(".ui-slider-handle ").click();
                 $(".ui-slider-handle ").sendKeys(Keys.ARROW_RIGHT);
             }
-            }else{
-                for (int i=0; i<5-moodRating;i++){
-                    $(".ui-slider-handle ").click();
-                    $(".ui-slider-handle ").sendKeys(Keys.ARROW_LEFT);}
+        }else{
+            for (int i=0; i<5-moodRating; i++){
+                $(".ui-slider-handle ").click();
+                $(".ui-slider-handle ").sendKeys(Keys.ARROW_LEFT);}
         }
         return this;
-        }
+    }
     public MoodModal clickUpdateMood(){
         $(UPDATE_BUTTON_CSS).click();
         return this;
